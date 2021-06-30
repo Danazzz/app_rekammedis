084227ca96cd36ffeb66d136b76c13e713fde3ee
Danazzz <artaputra95@gmail.com>
2021-06-30 13:14:25 +0800

    menghapus function yang tidak diperlukan

--- _config/config.php
+++ _config/config.php
@@ -1,22 +1,7 @@
 <?php
-// setting default timezone 
-date_default_timezone_set('asia/hong_kong');
-
-// session start
-session_start();
-
 // koneksi ke database
 $con=mysqli_connect('localhost','root','','rekam_medis');
 if(mysqli_connect_errno()){
     echo mysqli_connect_error();
-}
-
-// function untuk tgl format indonesia
-function tgl_indo($tgl){
-	$tanggal = substr($tgl, 8, 2);
-	$bulan = substr($tgl, 5, 2);
-	$tahun = substr($tgl, 0, 4);
-
-	return $tanggal."/".$bulan."/".$tahun;
 }
 ?>
